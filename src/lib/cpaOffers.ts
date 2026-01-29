bd6c4d27c81e78fb692db0fda0d5c5cbba9dc2ab
Alejandro Bartelloni <alex.dunno@gmail.com>
2026-01-29 15:13:23 -0300

    move CPA offers to env vars (android / ios / desktop)

--- src/lib/cpaOffers.ts
+++ src/lib/cpaOffers.ts
@@ -5,22 +5,26 @@ export type Offer = {
   name: string;
   url: string;
   allowed: DeviceOS[];
-  weight?: number;
 };
 
 export const OFFERS: Offer[] = [
   {
-    id: "mx_desktop_ios_69536",
-    name: "MX Desktop + iOS",
-    url: "https://singingfiles.com/show.php?l=0&u=2493983&id=69536",
-    allowed: ["desktop", "ios"],
-    weight: 10,
+    id: "mx_android",
+    name: "MX Android",
+    url: process.env.NEXT_PUBLIC_CPA_ANDROID_URL!,
+    allowed: ["android"],
   },
   {
-    id: "mx_android_66422",
-    name: "MX Android",
-    url: "https://singingfiles.com/show.php?l=0&u=2493983&id=66422",
-    allowed: ["android"],
-    weight: 10,
+    id: "mx_ios",
+    name: "MX iOS",
+    url: process.env.NEXT_PUBLIC_CPA_IOS_URL!,
+    allowed: ["ios"],
+  },
+  {
+    id: "mx_desktop",
+    name: "MX Desktop",
+    url: process.env.NEXT_PUBLIC_CPA_DESKTOP_URL!,
+    allowed: ["desktop"],
   },
 ];
+
